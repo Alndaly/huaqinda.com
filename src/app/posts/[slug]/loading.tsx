9aa6aa87e9ec97437b49179b9f66078beb3f579b
Kinda Hall <1142704468@qq.com>
2025-05-15 23:10:51 +0800

    feat: 🎸 优化loading页面样式

--- src/app/posts/[slug]/loading.tsx
+++ src/app/posts/[slug]/loading.tsx
@@ -3,10 +3,10 @@ import { Loader2 } from 'lucide-react';
 export default function Loading() {
 	// You can add any UI inside Loading, including a Skeleton.
 	return (
-		<div className='flex-1 flex justify-center items-center'>
-			<div className='flex flex-row items-center gap-2'>
-				<p>Content is loading...</p>
-				<Loader2 className='animate-spin h-8 w-8' />
+		<div className='flex justify-center items-center h-[calc(100vh-64px)]'>
+			<div className='flex flex-col items-center gap-2'>
+				<Loader2 className='animate-spin h-6 w-6' />
+				<p>Content is loading, please wait for a while...</p>
 			</div>
 		</div>
 	);

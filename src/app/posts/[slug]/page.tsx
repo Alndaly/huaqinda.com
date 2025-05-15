2c91cdefaf70de0e4544715a4372ae226fbdaa17
Kinda Hall <1142704468@qq.com>
2025-05-15 23:26:13 +0800

    feat: 🎸 补充一些组件

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { posts } from '#velite';
+import Comments from '@/components/comments';
 import { MDXContent } from '@/components/mdx-content';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 type Params = Promise<{ slug: string }>;
@@ -14,6 +16,15 @@ const PostPage = async ({ params }: { params: Params }) => {
 		<article className='prose dark:prose-invert mx-auto p-5 sm:py-10'>
 			<h1>{post.title}</h1>
 			<MDXContent code={post.code} />
+			<Link
+				className='no-underline'
+				href='https://creativecommons.org/licenses/by-nc-sa/4.0/'
+				target='_blank'>
+				<p className='py-2 text-sm text-right sm:text-left text-zinc-500 dark:text-zinc-400'>
+					CC BY-NC-SA 4.0
+				</p>
+			</Link>
+			<Comments />
 		</article>
 	);
 };

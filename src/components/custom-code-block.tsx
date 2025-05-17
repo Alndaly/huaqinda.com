91f150498abcbe96c50ca2b51dcf9e8505ff8012
Kinda Hall <1142704468@qq.com>
2025-05-17 12:51:04 +0800

    feat: 🎸 优化样式

--- src/components/custom-code-block.tsx
+++ src/components/custom-code-block.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { codeToHtml } from 'shiki';
 import { useEffect, useState } from 'react';
+import { Skeleton } from './ui/skeleton';
 
 const CustomCodeBlock = ({
 	children,
@@ -11,7 +12,6 @@ const CustomCodeBlock = ({
 	className: string;
 	isBlock?: boolean;
 }) => {
-	const [isClient, setIsClient] = useState(false);
 	const [html, setHtml] = useState<any>();
 	const handleInitCode = async () => {
 		const element = await codeToHtml(children, {
@@ -24,18 +24,17 @@ const CustomCodeBlock = ({
 		setHtml(element);
 	};
 	useEffect(() => {
-		setIsClient(true);
-	}, []);
-	useEffect(() => {
 		handleInitCode();
 	}, []);
-	if (!isClient) return null;
-
 	if (!isBlock) {
 		return <code>{children}</code>;
 	}
-
-	return <p dangerouslySetInnerHTML={{ __html: html }} />;
+	return (
+		<>
+			{html && <p dangerouslySetInnerHTML={{ __html: html }} />}
+			{!html && <Skeleton className='w-full h-15' />}
+		</>
+	);
 };
 
 export default CustomCodeBlock;
